7a40c6d9f6763bebd7fd3680dde9a4d195e0b54e
AshBakchod <114017724+AshBakchod@users.noreply.github.com>
2023-08-18 19:59:20 +0530

    Update BubbleSort.java

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -1,2 +1,15 @@
-package Sorting;public class BubbleSort {
+package Sorting;
+public class BubbleSort {
+   public static int[] bubblesort(int[] arr){
+        for(int i=arr.length-1;i>=0;i--){
+            for(int j=0;j<i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        return arr;
+    }
 }
