8e52dd5fc33bc2b8da79214c947186d37ca63fb3
AshBakchod <114017724+AshBakchod@users.noreply.github.com>
2023-08-18 20:00:53 +0530

    Update InsertionSort.java

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -1,2 +1,18 @@
-package Sorting;public class InsertionSort {
+package Sorting;
+public class InsertionSort {
+ public static int[] insertionsort(int[] arr){
+
+        for(int i=1;i<arr.length;i++){
+
+            int elem = arr[i];
+            int j = i;
+
+            while(j > 0 && arr[j-1]>elem) { // inserting elem
+                arr[j] = arr[j-1];
+                j--;
+            }
+            arr[j] = elem;
+        }
+        return arr;
+    }
 }
