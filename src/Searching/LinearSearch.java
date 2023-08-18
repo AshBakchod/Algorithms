a8e7e0fabe598c83fa029cc6962d91fc7df8dfe2
AshBakchod <114017724+AshBakchod@users.noreply.github.com>
2023-08-18 20:00:22 +0530

    Update LinearSearch.java

--- src/Searching/LinearSearch.java
+++ src/Searching/LinearSearch.java
@@ -1,2 +1,11 @@
-package Searching;public class LinearSearch {
+package Searching;
+public class LinearSearch {
+   public static int LinearSearch(int[] arr,int target){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
