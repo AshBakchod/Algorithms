40a74473ffb4865a6d4d32578a670ad4c83a3da3
AshBakchod <114017724+AshBakchod@users.noreply.github.com>
2023-08-18 20:02:23 +0530

    Update SelectionSort.java

--- src/Sorting/SelectionSort.java
+++ src/Sorting/SelectionSort.java
@@ -1,2 +1,17 @@
-package Sorting;public class SelectionSort {
+package Sorting;
+public class SelectionSort {
+   public static int[] selectionsort(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            int min = arr[i];
+            for(int j=i+1;j<arr.length;j++){
+                if(min>arr[j]){
+                    min=arr[j];
+                    arr[j]=arr[i];
+                    arr[i]=min;
+
+                }
+            }
+        }
+        return arr;
+    }
 }
