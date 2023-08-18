df63d5051c0294a7e87e37b7170f03f08336ab7e
AshBakchod <114017724+AshBakchod@users.noreply.github.com>
2023-08-18 19:59:50 +0530

    Update BinarySearch.java

--- src/Searching/BinarySearch.java
+++ src/Searching/BinarySearch.java
@@ -1,2 +1,17 @@
-package Searching;public class BinarySearch {
+package Searching;
+public class BinarySearch {
+   public static int binarysearch(int[] arr, int target){
+       for(int strt = 0, end = arr.length-1;strt<=end;){
+          int mid = (strt+end)/2;
+           if(arr[mid]>target){
+               end=mid-1;
+               continue;
+           } else if (arr[mid]<target) {
+               strt=mid+1;
+               continue;
+           }
+           return mid;
+       }
+       return -1;
+    }
 }
